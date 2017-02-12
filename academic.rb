5796288e4e5b84589b75afc2ec4997013b12c0f7
Diarmuid O Connor <doconnor@wit.ie>
2017-02-12 19:20:42 +0000

    Onitial structure

---
+++ academic.rb
@@ -0,0 +1,64 @@
+WKS_PER_SEMESTER = 12
+
+class Topic
+  attr_reader   :noLectures, :name 
+  
+  def initialize(noLectures, name)
+     @noLectures = noLectures
+     @name = name
+  end  
+  
+end
+
+class Subject    # aka Module
+  def initialize(name, noLectures, credits)
+     @noLecturesPerWeek = noLectures
+     @name = name
+     @credits = credits
+     @topics = []
+  end 
+
+  def findTopic name
+      @topics.find {|topic| topic.name == name }
+  end
+
+  def addTopic topic
+  	if (computeTopicTotal + topic.noLectures <= 
+                     WKS_PER_SEMESTER * @noLecturesPerWeek )
+       @topics << topic 
+       true
+    else
+  	   false
+    end
+  end
+
+  def removeTopic name
+    result = false
+    topic = findTopic name
+    if topic 
+       @topics.delete topic
+       result = true
+    end
+    return result
+  end
+  
+  def mergeTopics old_name1, old_name2, new_name
+      merged = nil
+      topic1 = findTopic(old_name1)
+      topic2 = findTopic(old_name2 )
+    if ( topic1 != nil && topic2 != nil )
+      merged = Topic.new( 
+          (topic1.noLectures + topic2.noLectures), new_name )
+      removeTopic topic1.name
+      removeTopic topic2.name
+      addTopic merged
+    end
+    return merged
+  end
+
+  private
+
+  def computeTopicTotal 
+  	@topics.inject(0) {|sum, topic| sum += topic.noLectures}
+  end
+end
---
+++ test-academic.rb
@@ -0,0 +1,65 @@
+require 'minitest/autorun'
+require_relative 'academic'
+
+class TestSubject < MiniTest::Test
+  def setup
+    @subject = Subject.new 'Module 1',2,5
+    @topic1 = Topic.new(6,"topic 1") ;
+    @topic2 = Topic.new(7,"topic 2") ;
+    @topic3 = Topic.new(5,"topic 3") ;;
+  end
+
+  def teardown
+      @subject = @topic1 = @topic2 = @topic3 = nil
+  end
+  
+  def test_find_topic
+      addSomeTopics
+      # Normal
+      result = @subject.findTopic 'topic 2'
+      assert_equal @topic2, result
+      result = @subject.findTopic 'topic 4'
+      assert_nil result
+   end
+
+  def test_add_topic
+        # Normal
+        result = @subject.addTopic @topic1
+        assert result
+        assert_equal @topic1, @subject.findTopic('topic 1')
+        # Extra setup
+        @subject.addTopic @topic2
+        @subject.addTopic @topic3        
+        # Error
+        topic4 = Topic.new 20, 'topic 4'
+        result = @subject.addTopic topic4
+        assert_equal false, result
+        result = @subject.findTopic 'topic 4'
+        assert_nil result
+        # Boundry
+        topic4 = Topic.new 6, 'topic 4'
+        result = @subject.addTopic topic4
+        assert result
+        assert_equal topic4, @subject.findTopic('topic 4')
+   end
+   
+   def test_remove_topic
+     addSomeTopics
+     # Normal
+     result = @subject.removeTopic 'topic 1'
+     assert result
+     assert_nil @subject.findTopic 'topic 1'
+     result = @subject.removeTopic 'topic X'
+     assert_equal false, result
+   end
+
+   def test_merge_topics
+     # To be completed
+   end
+
+   def addSomeTopics
+      @subject.addTopic @topic1
+      @subject.addTopic @topic2
+      @subject.addTopic @topic3
+  end
+end
